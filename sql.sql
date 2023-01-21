a834ef3cdaefadd8f86d4ab0d20bb1de98d8e564
Alexander Riccardo Zimmermann <alexander.riccardo.z@gmail.com>
2023-01-21 19:00:58 +0100

    added SQLite database

---
+++ sql.sql
@@ -0,0 +1,19 @@
+/* 
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT NOT NULL PRIMARY KEY,
+    display TEXT NOT NULL,
+    password TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+*/
+
+/* 
+DROP TABLE IF EXISTS users;
+*/
+
+/* 
+INSERT INTO users (username, display, password) 
+VALUES ('admin', 'Admin', '1');
+*/
+
+
