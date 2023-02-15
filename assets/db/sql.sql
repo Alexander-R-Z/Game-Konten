3c187a2776613310d30c33eac06cc3fdd89cf794
Alexander Riccardo Zimmermann <alexander.riccardo.z@gmail.com>
2023-02-15 21:15:36 +0100

    neue Datenbank Tabellen (verbessert) Datenbank Daten gelöscht Admin added dynamic form valiu first step Modalpopup-Fenster erste idee

--- assets/db/sql.sql
+++ assets/db/sql.sql
@@ -1,92 +1,197 @@
 -- control + shift + p -> SQL: Run SQL File
 
 /* 
+Old SQLite Tables
 CREATE TABLE IF NOT EXISTS User (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
     displayname TEXT NOT NULL,
     password TEXT NOT NULL,
     createDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
     changeDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
     lastLoginDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
-    PRIMARY KEY (username),
     CHECK (length(username) <= 50),
     CHECK (length(displayname) <= 50),
     CHECK (length(password) <= 2000)
 );
 
 CREATE TABLE IF NOT EXISTS Role (
-    roleName TEXT NOT NULL,
+    rolename TEXT NOT NULL,
     createDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
     createdBy TEXT NOT NULL,
     changeDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
     changedBy TEXT,
-    PRIMARY KEY (roleName),
-    CHECK (length(roleName) <= 20)
+    PRIMARY KEY (rolename),
+    CHECK (length(rolename) <= 20)
 );
 
 CREATE TABLE IF NOT EXISTS UserRole (
-    username TEXT NOT NULL,
-    roleName TEXT NOT NULL,
-    PRIMARY KEY (username, roleName),
-    FOREIGN KEY (username) REFERENCES User (username),
-    FOREIGN KEY (roleName) REFERENCES Role (roleName)
+    usernameId INT NOT NULL,
+    rolename TEXT NOT NULL,
+    PRIMARY KEY (usernameId, rolename),
+    FOREIGN KEY (usernameId) REFERENCES User (id),
+    FOREIGN KEY (rolename) REFERENCES Role (rolename)
 );
 
 CREATE TABLE IF NOT EXISTS Game (
-    gameName TEXT NOT NULL,
+    gamename TEXT NOT NULL,
     createDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
     createdBy TEXT NOT NULL,
     changeDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
     changedBy TEXT,
-    PRIMARY KEY (gameName),
-    CHECK (length(gameName) <= 50)
+    PRIMARY KEY (gamename),
+    CHECK (length(gamename) <= 50)
 );
 
 CREATE TABLE IF NOT EXISTS AccountData (
-    userName TEXT NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
     password TEXT NOT NULL,
     displayname TEXT,
     tagline TEXT,
-    gameName TEXT NOT NULL,
+    gamename TEXT NOT NULL,
     createDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
     createdBy TEXT NOT NULL,
     changeDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
     changedBy TEXT,
-    PRIMARY KEY (userName),
-    FOREIGN KEY (gameName) REFERENCES Game(gameName),
-    CHECK (length(userName) <= 50),
+    FOREIGN KEY (gamename) REFERENCES Game(gamename),
+    CHECK (length(username) <= 50),
     CHECK (length(password) <= 50),
     CHECK (length(tagline) <= 10)
 );
 
 CREATE TABLE IF NOT EXISTS RoleAccountData (
-    roleName TEXT NOT NULL REFERENCES Role(roleName),
-    userName TEXT NOT NULL REFERENCES AccountData(userName),
-    PRIMARY KEY (roleName, userName)
+    rolename TEXT NOT NULL REFERENCES Role(rolename),
+    accountId INT NOT NULL REFERENCES AccountData(id),
+    PRIMARY KEY (rolename, accountId)
 );
+Old SQLite Tables
 */
 
-/* 
-DROP TABLE IF EXISTS User;
-DROP TABLE IF EXISTS Role;
+
+/*
+-- Drop the existing tables
 DROP TABLE IF EXISTS UserRole;
-DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS AccountData;
+DROP TABLE IF EXISTS Role;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS RoleAccountData;
+
+-- Create the new User table
+CREATE TABLE IF NOT EXISTS User (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL UNIQUE,
+displayname TEXT NOT NULL,
+password TEXT NOT NULL,
+createDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
+changeDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
+lastLoginDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
+CHECK (length(username) <= 50),
+CHECK (length(displayname) <= 50),
+CHECK (length(password) <= 2000)
+);
+
+-- Create the new Role table
+CREATE TABLE IF NOT EXISTS Role (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+rolename TEXT NOT NULL UNIQUE,
+createDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
+createdBy TEXT NOT NULL,
+changeDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
+changedBy TEXT,
+CHECK (length(rolename) <= 20)
+);
+
+-- Create the new Game table
+CREATE TABLE IF NOT EXISTS Game (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+gamename TEXT NOT NULL UNIQUE,
+createDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
+createdBy TEXT NOT NULL,
+changeDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
+changedBy TEXT,
+CHECK (length(gamename) <= 50)
+);
+
+-- Create the new AccountData table
+CREATE TABLE IF NOT EXISTS AccountData (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL UNIQUE,
+password TEXT NOT NULL,
+displayname TEXT,
+tagline TEXT,
+gamenameId INT NOT NULL,
+createDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
+createdBy TEXT NOT NULL,
+changeDate TEXT NOT NULL DEFAULT (datetime('now','localtime')),
+changedBy TEXT,
+FOREIGN KEY (gamenameId) REFERENCES Game (id) ON DELETE CASCADE,
+CHECK (length(username) <= 50),
+CHECK (length(password) <= 2000),
+CHECK (length(tagline) <= 10)
+);
+
+-- Create the new RoleAccountData table
+CREATE TABLE IF NOT EXISTS RoleAccountData (
+roleId INT NOT NULL,
+accountDataId INT NOT NULL,
+PRIMARY KEY (roleId, accountDataId),
+FOREIGN KEY (roleId) REFERENCES Role (id) ON DELETE CASCADE,
+FOREIGN KEY (accountDataId) REFERENCES AccountData (id) ON DELETE CASCADE
+);
+
+-- Create the new UserRole table
+CREATE TABLE IF NOT EXISTS UserRole (
+userId INT NOT NULL,
+roleId INT NOT NULL,
+PRIMARY KEY (userId, roleId),
+FOREIGN KEY (userId) REFERENCES User (id) ON DELETE CASCADE,
+FOREIGN KEY (roleId) REFERENCES Role (id) ON DELETE CASCADE
+);
+
+-- Create indexes to improve query performance
+CREATE INDEX idx_user_username ON User(username);
+CREATE INDEX idx_role_rolename ON Role(rolename);
+CREATE INDEX idx_game_gamename ON Game(gamename);
 */
 
 /* 
-INSERT INTO User (username, displayname, password, createDate, changeDate, lastLoginDate)
-VALUES ('user1', 'User One', 'password123', datetime('now'), datetime('now'), datetime('now'));
+INSERT INTO User (username, displayname, password) VALUES ('john_doe', 'John Doe', 'mypassword');
+-- Fügt einen neuen Benutzer hinzu, der sich als 'john_doe' mit dem Anzeigenamen 'John Doe' und dem Passwort 'mypassword' registriert hat.
 
-INSERT INTO Role (roleName, createDate, createdBy, changeDate, changedBy)
-VALUES ('Admin', DATETIME('now'), 'User1', DATETIME('now'), 'User1');
+INSERT INTO Role (rolename, createdBy) VALUES ('Admin', 'System');
+-- Fügt eine neue Rolle mit dem Namen 'Admin' und dem Ersteller 'System' hinzu.
 
-INSERT INTO Game (gameName, createDate, createdBy, changeDate, changedBy)
-VALUES ('Game1', DATETIME('now'), 'User1', DATETIME('now'), 'User1');
+INSERT INTO Game (gamename, createdBy) VALUES ('Chess', 'System');
+-- Fügt ein neues Spiel mit dem Namen 'Chess' und dem Ersteller 'System' hinzu.
 
-INSERT INTO AccountData (userName, password, displayname, tagline, createDate, createdBy, changeDate, changedBy, gameId)
-VALUES ('User1', 'password1', 'Display Name 1', 'Tagline 1', DATETIME('now'), 'User1', DATETIME('now'), 'User1', 1);
+INSERT INTO AccountData (username, password, gamenameId, createdBy) VALUES ('john_doe', 'mypassword', 1, 'System');
+-- Fügt neue Kontodaten für einen Benutzer mit dem Benutzernamen 'john_doe', dem Passwort 'mypassword' und dem Spiel mit der ID 1 hinzu.
+
+INSERT INTO RoleAccountData (roleId, accountDataId) VALUES (1, 1);
+-- Fügt eine Zuordnung zwischen der Rolle mit der ID 1 und den Kontodaten mit der ID 1 hinzu.
+
+INSERT INTO UserRole (userId, roleId) VALUES (1, 1);
+-- Fügt eine Zuordnung zwischen dem Benutzer mit der ID 1 und der Rolle mit der ID 1 hinzu.
 */
 
+/*
+SELECT * FROM User WHERE id = 1;
+-- Gibt alle Spalten und Zeilen der User-Tabelle zurück, wo die id 1 ist.
 
+SELECT * FROM Role WHERE rolename = 'Admin';
+-- Gibt alle Spalten und Zeilen der Role-Tabelle zurück, wo das rolename 'Admin' ist.
+
+SELECT * FROM Game WHERE gamename = 'Chess';
+-- Gibt alle Spalten und Zeilen der Game-Tabelle zurück, wo das gamename 'Chess' ist.
+
+SELECT * FROM AccountData WHERE username = 'john_doe';
+-- Gibt alle Spalten und Zeilen der AccountData-Tabelle zurück, wo der username 'john_doe' ist.
+
+SELECT * FROM RoleAccountData WHERE roleId = 1;
+-- Gibt alle Spalten und Zeilen der RoleAccountData-Tabelle zurück, wo die roleId 1 ist.
+
+SELECT * FROM UserRole WHERE userId = 1;
+-- Gibt alle Spalten und Zeilen der UserRole-Tabelle zurück, wo die userId 1 ist.
+*/
